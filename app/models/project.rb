22c1d8c31f5737c7cf20c3c83196271c7a09282d
root <bwarren2@uccs.edu>
2021-11-15 21:33:21 +0000

    Confirmed that CircleCI testing works

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ApplicationRecord
     # Require that the project has a title and a description
-    #validates_presence_of :title, :description
+    validates_presence_of :title, :description
 end
